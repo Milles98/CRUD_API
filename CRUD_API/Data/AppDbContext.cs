201f61d2c9a425166da66842c3224b9f9cfa8816
Mille <Mille@DESKTOP-5S71H0A>
2025-06-17 14:17:38 +0200

    Add AppDbContext and SQLite database setup
    Implemented AppDbContext for managing database entities including Customers, Orders, Payments, and Products. Configured the application to use a SQLite database named checkout.db in Program.cs.
    
    Created migration class 20250617121405_Init to define the database schema with necessary tables, foreign key constraints, and indexes. Updated migration designer and model snapshot to reflect the new schema for Entity Framework Core.

--- CRUD_API/Data/AppDbContext.cs
+++ CRUD_API/Data/AppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace CRUD_API.Data
+﻿using CRUD_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_API.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Order> Orders => Set<Order>();
+        public DbSet<Payment> Payments => Set<Payment>();
+        public DbSet<Product> Products => Set<Product>();
     }
 }
--- CRUD_API/Program.cs
+++ CRUD_API/Program.cs
@@ -1,3 +1,6 @@
+using CRUD_API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlite("Data Source=checkout.db"));
 
 var app = builder.Build();
 
