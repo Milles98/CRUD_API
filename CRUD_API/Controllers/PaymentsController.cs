7419c1e1c8adb11bb57e3c12646dfc8dd87b42d1
Mille <Mille@DESKTOP-5S71H0A>
2025-06-24 13:38:34 +0200

    Created getallpayments endpoint with DTOs

--- CRUD_API/Controllers/PaymentsController.cs
+++ CRUD_API/Controllers/PaymentsController.cs
@@ -1,10 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRUD_API.Data;
+using CRUD_API.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUD_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PaymentsController : ControllerBase
+    public class PaymentsController(AppDbContext context) : ControllerBase
     {
+        [HttpGet]
+        public async Task<IActionResult> GetAllPayments()
+        {
+            var payments = await context.Payments
+                .ToListAsync();
+
+            if(payments.Count == 0)
+                return Ok(new List<PaymentResponseDto>());
+
+            var paymentDto = payments.Select(payment => new PaymentResponseDto
+            {
+                Id = payment.Id,
+                AttemptedAt = payment.AttemptedAt,
+                Amount = payment.Amount,
+                Method = payment.Method,
+                OrderId = payment.OrderId,
+                Status = payment.Status
+            });
+
+            return Ok(paymentDto);
+        }
     }
 }
---
+++ CRUD_API/DTO/PaymentResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CRUD_API.DTO
+{
+    public class PaymentResponseDto
+    {
+        public int Id { get; set; }
+        public DateTime AttemptedAt { get; set; }
+        public decimal Amount { get; set; }
+        public string? Method { get; set; }
+        public string? Status { get; set; }
+        public int OrderId { get; set; }
+    }
+}
